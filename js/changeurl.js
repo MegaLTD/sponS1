7271dd7f0971636f15f7db9a9eda496bc17b2465
MegaWatchLTD <102382969+MegaWatchLTD@users.noreply.github.com>
2022-08-10 18:00:53 +0100

    Add files via upload

---
+++ js/changeurl.js
@@ -0,0 +1,178 @@
+function changeurl1(){
+    let url0000000001 = "../La-Casa-De-Papel-S05/Episodes/Ep1/server1/play.html";
+    let url000000001 = "../La-Casa-De-Papel-S05/Episodes/Ep2/server1/play.html";
+    let url00000001 = "../La-Casa-De-Papel-S05/Episodes/Ep3/server1/play.html";
+    let url0000001 = "../La-Casa-De-Papel-S05/Episodes/Ep4/server1/play.html";
+    let url000001 = "../La-Casa-De-Papel-S05/Episodes/Ep5/server1/play.html";
+    let url00001 = "../La-Casa-De-Papel-S05/Episodes/Ep6/server1/play.html";
+    let url0001 = "../La-Casa-De-Papel-S05/Episodes/Ep7/server1/play.html";
+    let url001 = "../La-Casa-De-Papel-S05/Episodes/Ep8/server1/play.html";
+    let url01 = "../La-Casa-De-Papel-S05/Episodes/Ep9/server1/play.html";
+    let url1 = "../La-Casa-De-Papel-S05/Episodes/Ep10/server1/play.html";
+
+    document.getElementById("Ep1-1").href=url0000000001;
+    document.getElementById("Ep1-2").href=url0000000001;
+
+    document.getElementById("Ep2-1").href=url000000001;
+    document.getElementById("Ep2-2").href=url000000001;
+
+    document.getElementById("Ep3-1").href=url00000001;
+    document.getElementById("Ep3-2").href=url00000001;
+
+    document.getElementById("Ep4-1").href=url0000001;
+    document.getElementById("Ep4-2").href=url0000001;
+
+    document.getElementById("Ep5-1").href=url000001;
+    document.getElementById("Ep5-2").href=url000001;
+
+    document.getElementById("Ep6-1").href=url00001;
+    document.getElementById("Ep6-2").href=url00001;
+
+    document.getElementById("Ep7-1").href=url0001;
+    document.getElementById("Ep7-2").href=url0001;
+
+    document.getElementById("Ep8-1").href=url001;
+    document.getElementById("Ep8-2").href=url001;
+
+    document.getElementById("Ep9-1").href=url01;
+    document.getElementById("Ep9-2").href=url01;
+
+    document.getElementById("Ep10-1").href=url1;
+    document.getElementById("Ep10-2").href=url1;
+
+}
+
+
+function changeurl2(){
+    let url0000000002 = "../La-Casa-De-Papel-S05/Episodes/Ep1/server2/play.html";
+    let url000000002 = "../La-Casa-De-Papel-S05/Episodes/Ep2/server2/play.html";
+    let url00000002 = "../La-Casa-De-Papel-S05/Episodes/Ep3/server2/play.html";
+    let url0000002 = "../La-Casa-De-Papel-S05/Episodes/Ep4/server2/play.html";
+    let url000002 = "../La-Casa-De-Papel-S05/Episodes/Ep5/server2/play.html";
+    let url00002 = "../La-Casa-De-Papel-S05/Episodes/Ep6/server2/play.html";
+    let url0002 = "../La-Casa-De-Papel-S05/Episodes/Ep7/server2/play.html";
+    let url002 = "../La-Casa-De-Papel-S05/Episodes/Ep8/server2/play.html";
+    let url02 = "../La-Casa-De-Papel-S05/Episodes/Ep9/server2/play.html";
+    let url2 = "../La-Casa-De-Papel-S05/Episodes/Ep10/server2/play.html";
+
+    document.getElementById("Ep1-1").href=url0000000002;
+    document.getElementById("Ep1-2").href=url0000000002;
+
+    document.getElementById("Ep2-1").href=url000000002;
+    document.getElementById("Ep2-2").href=url000000002;
+
+    document.getElementById("Ep3-1").href=url00000002;
+    document.getElementById("Ep3-2").href=url00000002;
+
+    document.getElementById("Ep4-1").href=url0000002;
+    document.getElementById("Ep4-2").href=url0000002;
+
+    document.getElementById("Ep5-1").href=url000002;
+    document.getElementById("Ep5-2").href=url000002;
+
+    document.getElementById("Ep6-1").href=url00002;
+    document.getElementById("Ep6-2").href=url00002;
+
+    document.getElementById("Ep7-1").href=url0002;
+    document.getElementById("Ep7-2").href=url0002;
+
+    document.getElementById("Ep8-1").href=url002;
+    document.getElementById("Ep8-2").href=url002;
+
+    document.getElementById("Ep9-1").href=url02;
+    document.getElementById("Ep9-2").href=url02;
+
+    document.getElementById("Ep10-1").href=url2;
+    document.getElementById("Ep10-2").href=url2;
+
+}
+
+
+function changeurl3(){
+    let url0000000003 = "../La-Casa-De-Papel-S05/Episodes/Ep1/server3/play.html";
+    let url000000003 = "../La-Casa-De-Papel-S05/Episodes/Ep2/server3/play.html";
+    let url00000003 = "../La-Casa-De-Papel-S05/Episodes/Ep3/server3/play.html";
+    let url0000003 = "../La-Casa-De-Papel-S05/Episodes/Ep4/server3/play.html";
+    let url000003 = "../La-Casa-De-Papel-S05/Episodes/Ep5/server3/play.html";
+    let url00003 = "../La-Casa-De-Papel-S05/Episodes/Ep6/server1/play.html";
+    let url0003 = "../La-Casa-De-Papel-S05/Episodes/Ep7/server3/play.html";
+    let url003 = "../La-Casa-De-Papel-S05/Episodes/Ep8/server3/play.html";
+    let url03 = "../La-Casa-De-Papel-S05/Episodes/Ep9/server3/play.html";
+    let url3 = "../La-Casa-De-Papel-S05/Episodes/Ep10/server3/play.html";
+
+    document.getElementById("Ep1-1").href=url0000000003;
+    document.getElementById("Ep1-2").href=url0000000003;
+
+    document.getElementById("Ep2-1").href=url000000003;
+    document.getElementById("Ep2-2").href=url000000003;
+
+    document.getElementById("Ep3-1").href=url00000003;
+    document.getElementById("Ep3-2").href=url00000003;
+
+    document.getElementById("Ep4-1").href=url0000003;
+    document.getElementById("Ep4-2").href=url0000003;
+
+    document.getElementById("Ep5-1").href=url000003;
+    document.getElementById("Ep5-2").href=url000003;
+
+    document.getElementById("Ep6-1").href=url00003;
+    document.getElementById("Ep6-2").href=url00003;
+
+    document.getElementById("Ep7-1").href=url0003;
+    document.getElementById("Ep7-2").href=url0003;
+
+    document.getElementById("Ep8-1").href=url003;
+    document.getElementById("Ep8-2").href=url003;
+
+    document.getElementById("Ep9-1").href=url03;
+    document.getElementById("Ep9-2").href=url03;
+
+    document.getElementById("Ep10-1").href=url3;
+    document.getElementById("Ep10-2").href=url3;
+
+}
+
+
+function changeurl4(){
+    let url0000000004 = "../La-Casa-De-Papel-S05/Episodes/Ep1/server4/play.html";
+    let url000000004 = "../La-Casa-De-Papel-S05/Episodes/Ep2/server4/play.html";
+    let url00000004 = "../La-Casa-De-Papel-S05/Episodes/Ep3/server4/play.html";
+    let url0000004 = "../La-Casa-De-Papel-S05/Episodes/Ep4/server4/play.html";
+    let url000004 = "../La-Casa-De-Papel-S05/Episodes/Ep5/server4/play.html";
+    let url00004 = "../La-Casa-De-Papel-S05/Episodes/Ep6/server4/play.html";
+    let url0004 = "../La-Casa-De-Papel-S05/Episodes/Ep7/server4/play.html";
+    let url004 = "../La-Casa-De-Papel-S05/Episodes/Ep8/server4/play.html";
+    let url04 = "../La-Casa-De-Papel-S05/Episodes/Ep9/server4/play.html";
+    let url4 = "../La-Casa-De-Papel-S05/Episodes/Ep10/server4/play.html";
+
+    document.getElementById("Ep1-1").href=url0000000004;
+    document.getElementById("Ep1-2").href=url0000000004;
+
+    document.getElementById("Ep2-1").href=url000000004;
+    document.getElementById("Ep2-2").href=url000000004;
+
+    document.getElementById("Ep3-1").href=url00000004;
+    document.getElementById("Ep3-2").href=url00000004;
+
+    document.getElementById("Ep4-1").href=url0000004;
+    document.getElementById("Ep4-2").href=url0000004;
+
+    document.getElementById("Ep5-1").href=url000004;
+    document.getElementById("Ep5-2").href=url000004;
+
+    document.getElementById("Ep6-1").href=url00004;
+    document.getElementById("Ep6-2").href=url00004;
+
+    document.getElementById("Ep7-1").href=url0004;
+    document.getElementById("Ep7-2").href=url0004;
+
+    document.getElementById("Ep8-1").href=url004;
+    document.getElementById("Ep8-2").href=url004;
+
+    document.getElementById("Ep9-1").href=url04;
+    document.getElementById("Ep9-2").href=url04;
+
+    document.getElementById("Ep10-1").href=url4;
+    document.getElementById("Ep10-2").href=url4;
+
+}
